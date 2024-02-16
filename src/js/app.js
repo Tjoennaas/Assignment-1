f8a9c404de9a90b24b5a4214922126eae2ec9e11
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2024-02-16 21:27:24 +0100

    renderData and crate elements

--- src/js/app.js
+++ src/js/app.js
@@ -18,11 +18,11 @@
         };
 
         const recordImages = { //Adding image to each of the items.
-                  "Luke Skywalker": "src/7.jpg",
-                  "Darth Vader": "src/6.jpg",
+                  "Luke Skywalker": "src/.jpg",
+                  "Darth Vader": "src/.jpg",
         };
 
-// Selecting all the buttons in the categories, to perform actions on each button.
+// Selecting all the buttons, to perform actions on each button.
         const categoryButtons = document.querySelectorAll('.category__button')
         categoryButtons.forEach(button =>{  
         button.addEventListener('click', async () => {  // The async will execute data from swapiAPI,when butons are activ.
@@ -32,7 +32,7 @@
        const backButton = document.querySelector( '.backButton')
        const contentContainer = document.querySelector('[data-to-show]')
 
-// Use classList to toggle visibility
+//Toggle visibility
        category.classList.add( 'hidden')
        backButton.classList.remove('hidden')
        contentContainer.classList.remove('hidden')
@@ -45,8 +45,8 @@
 
 
         try {
-       const response = await fetch(endpointsUrl); // This line sends a network request to the URL contained in the endpoints variable
-//The await keyword pauses execution of the code in this async function until the fetch request is resolved.
+       const response = await fetch(endpointsUrl); // This line sends a network request to the URL contained in the endpoints variable.
+//The await keyword pauses execution of the code in this async function, until the fetch request is resolved.
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);//checks if the response was successful.
         const data = await response.json();
         renderData(data.results, endpointsUrl);
@@ -56,4 +56,26 @@
         });
      });  
 
+   const renderData = (data, dataTarget) => {  //contentContainer for displaying a list based on data.
+       const contentContainer = document.querySelector('[data-to-show]');  
+       contentContainer.innerText = ''; //Empty the content of this element by setting its innerText to an empty string.
+       const list = document.createElement( 'ul'); //Create a  <ul> element that can be used to display a list based on the data.
+   }
+   if (records[dataTarget].length > 0) {   // Data is filtered to include only those items whose name or title matches items in records[dataTarget].
+       filteredData = data.filter(item => records[dataTarget].includes(item.name || item.title));
+   }
+     
+   filteredData.forEach(item => {
+       const listItem = document.createElement('li');
+       const image = document.createElement('img');
+       const textContainer = document.createElement('div');
+       const itemName = document.createElement('p');
+       const itemProperties = document.createElement('p');
 
+// provides description of the image for screen readers used by visually impaired users.
+// Serves as a placeholder if the image fails to load.
+       itemName.textContent = item.name || item.title;
+       image.alt = itemName.textContent;
+    
+
+   })
